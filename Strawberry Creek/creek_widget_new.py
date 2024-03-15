7f4bc7530e7523030898f1daea2f3d7dcd1fee2a
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2024-03-15 15:08:16 -0700

    update script

--- Strawberry Creek/creek_widget_new.py
+++ Strawberry Creek/creek_widget_new.py
@@ -10,7 +10,7 @@ plt.style.use('fivethirtyeight')
 def mean_difference(stat, data):
     
     def difference_in_means(pd_series):
-        return abs(pd_series['North'].mean() - pd_series['South'].mean())
+        return abs(pd_series['North'].mean(numeric_only=True) - pd_series['South'].mean(numeric_only=True))
 
     n_repeats = 1000
     permutation_differences = np.array([])
@@ -22,7 +22,7 @@ def mean_difference(stat, data):
         new_difference = np.array(difference_in_means(permutation))
         permutation_differences = np.append(permutation_differences, new_difference)
         
-    observed_difference = abs(data[data['Fork']=='North'].mean() - data[data['Fork']=='South'].mean())
+    observed_difference = abs(data[data['Fork']=='North'].mean(numeric_only=True) - data[data['Fork']=='South'].mean(numeric_only=True))
     p_val_count = sum(i > observed_difference[stat] for i in permutation_differences)/len(permutation_differences)
     
     plt.figure(figsize=(12,4))
@@ -32,8 +32,8 @@ def mean_difference(stat, data):
     dataSouth = data[data['Fork'] == 'South']
     plt.hist(dataNorth[stat], color='blue', alpha=0.5, label='North')
     plt.hist(dataSouth[stat], color='gold', alpha=0.5, label='South')
-    plt.axvline(dataNorth[stat].mean(), color='blue', linestyle='--', label='North Mean')
-    plt.axvline(dataSouth[stat].mean(), color='gold', linestyle='--', label='South Mean')
+    plt.axvline(dataNorth[stat].mean(numeric_only=True), color='blue', linestyle='--', label='North Mean')
+    plt.axvline(dataSouth[stat].mean(numeric_only=True), color='gold', linestyle='--', label='South Mean')
     plt.xlabel(str(stat))
     plt.ylabel('Frequency')
     plt.title(str(stat) + " for North and South Fork")
